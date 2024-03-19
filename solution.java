5e79ec0d02f081d1593b0c23902e8273c620946b
Kooreis <kooreis.official@gmail.com>
2024-03-19 14:39:20 -0500

    In this update, the main method is created. It prompts the user to input the length of the array and the elements of the array. The array is then populated with the user's input.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that solves the problem:
-
-```java
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -15,34 +12,5 @@ public class Main {
         for (int i = 0; i < length; i++) {
             nums[i] = scanner.nextInt();
         }
-        System.out.println("The length of the longest consecutive sequence is: " + longestConsecutive(nums));
-    }
-
-    public static int longestConsecutive(int[] nums) {
-        Set<Integer> num_set = new HashSet<Integer>();
-        for (int num : nums) {
-            num_set.add(num);
-        }
-
-        int longestStreak = 0;
-
-        for (int num : num_set) {
-            if (!num_set.contains(num-1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (num_set.contains(currentNum+1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
-                }
-
-                longestStreak = Math.max(longestStreak, currentStreak);
-            }
-        }
-
-        return longestStreak;
     }
 }
-```
-
-This console application first asks the user to input the length of the array and then the elements of the array. It then calls the `longestConsecutive` method to find the length of the longest consecutive sequence in the array and prints the result. The `longestConsecutive` method uses a HashSet to eliminate duplicates and to check for the existence of elements in constant time. It iterates over each element in the set and checks if it is the first element of a sequence. If it is, it then checks how long the sequence is. The length of the longest sequence is then returned.
