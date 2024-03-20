4d849d3e30561f67edfa1bbb52121e679863e09b
Kooreis <kooreis.official@gmail.com>
2024-03-20 14:39:20 -0500

    In this update, the longestConsecutive method is created. It takes an array of integers as input and creates a HashSet from the array to eliminate duplicates.

--- solution.java
+++ solution.java
@@ -1,16 +1,6 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the length of the array:");
-        int length = scanner.nextInt();
-        int[] nums = new int[length];
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < length; i++) {
-            nums[i] = scanner.nextInt();
+public static int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
         }
-    }
 }
